2ba5052cf3526b7033de685e2ebb6bcbc06c8bdb
Kraml Liu <kraml.liu@gmail.com>
2010-08-09 19:31:24 +0800

    - All style definition use addGlobalCSS() instead of inline text. - Replaced single quote to double quote for strings for consistency.

--- quick_img_browsing.user.js
+++ quick_img_browsing.user.js
@@ -61,17 +61,70 @@ var $ = function(o){ return document.querySelectorAll(o); }
 function init()
 {
 	if(!initialized) {
-		addGlobalStyle("img[tabIndex=\"0\"] { border-style: solid !important; border-width: " + userprefs.borderWidth + "px" +
-						" !important; border-color: " + userprefs.curBorderColor + " !important;}");
-		addGlobalStyle("img[tabIndex=\"0\"].resized { border-style: solid !important; border-width: " + userprefs.borderWidth + "px" +
-						" !important; border-color: " + userprefs.resizedBorderColor + " !important;}");
+		addGlobalStyle("img[tabIndex='0'] { border-style: solid !important; " +
+											"border-width: " + userprefs.borderWidth + "px" + " !important; " +
+											"border-color: " + userprefs.curBorderColor + " !important; }");
+
+		addGlobalStyle("img[tabIndex=\"0\"].resized { border-style: solid !important; " +
+													"border-width: " + userprefs.borderWidth + "px" + " !important; " +
+													"border-color: " + userprefs.resizedBorderColor + " !important; }");
+
+		addGlobalStyle("div.sizeNoticeDIV { position: absolute !important; " +
+										"z-index: 2147483647 !important; " +
+										"float: none !important; " +
+										"font-family: Arial, Helvetica !important; " +
+										"font-size: 9pt !important; " +
+										"height: auto !important; " +
+										"width: auto !important; " +
+										"line-height: 16px !important; " +
+										"padding: 3px 1px 4px 1px!important; " +
+										"margin: 0px !important; " +
+										"border: 0 none !important; " +
+										"text-align: left !important; " +
+										"color: #666666 !important;" +
+										"background-color: " + userprefs.curBorderColor + " !important; }");
+
+		addGlobalStyle("span.sizeBtnSpan { cursor: pointer !important; " +
+										"height: auto !important; " +
+										"width: auto !important; " +
+										"line-height: 16px !important; " +
+										"background-color: #F1F1F1 !important; " +
+										"border: 0 none !important; " +
+										"margin: 2px !important; " +
+										"padding: 1px 3px 1px 2px !important; }");
+
+		addGlobalStyle("div.debugDIV { position: fixed !important; " +
+									"z-index: 2147483647 !important; " +
+									"float: none !important; " +
+									"font-family: Arial, Helvetica !important; " +
+									"font-size: 12px !important; " +
+									"padding: 2px 5px 2px 5px !important; " +
+									"background-color: #AF9C90 !important; " +
+									"border: none !important; " +
+									"text-align: left !important; " +
+									"color: #303030 !important; " +
+									"right: 0 !important; " +
+									"bottom: 0 !important; }");
+
+		addGlobalStyle("div.alertDIV { position: fixed !important; " +
+									"z-index: 2147483647 !important; " +
+									"float: none !important; " +
+									"font-family: Arial, Helvetica !important; " +
+									"font-size: 16px !important; " +
+									"padding: 2px 8px 2px 8px !important; " +
+									"border: none !important; " +
+									"text-align: left !important; " +
+									"color: black !important; " +
+									"left: 0 !important; " +
+									"bottom: 0 !important;" +
+									"background-color: " + userprefs.curBorderColor + " !important; }");
 
 		initialized = true;
 	}
 }
 
-document.addEventListener('keypress', function(event) {
-	if(event.charCode == userprefs.keyDown || event.charCode == userprefs.keyUp) { 
+document.addEventListener("keypress", function(event) {
+	if(event.charCode == userprefs.keyDown || event.charCode == userprefs.keyUp) {
 		init();
 
 		// Initialize the imgList every time when key pressed so if page changed(like with autopager) we will find the new images.
@@ -98,7 +151,7 @@ document.addEventListener('keypress', function(event) {
 				// Process image size, add border, event listener etc.
 				processImg(curImg);
 
-				if(DEBUG) { debugMsg("Image: " + imgIdx + " / " + imgList.length + 
+				if(DEBUG) { debugMsg("Image: " + imgIdx + " / " + imgList.length +
 									"</br>.X/.Y: " + curImg.x + " / " + curImg.y +
 									"</br>getX/getY: " + getX(curImg) + " / " + getY(curImg) +
 									"</br>Max(HxW): " + MAX_IMG_H + " x " + MAX_IMG_W +
@@ -133,7 +186,7 @@ document.addEventListener('keypress', function(event) {
 				// Process image size, add border, event listener etc.
 				processImg(curImg);
 
-				if(DEBUG) { debugMsg("Image: " + imgIdx + " / " + imgList.length + 
+				if(DEBUG) { debugMsg("Image: " + imgIdx + " / " + imgList.length +
 									"</br>.X/.Y: " + curImg.x + " / " + curImg.y +
 									"</br>getX/getY: " + getX(curImg) + " / " + getY(curImg) +
 									"</br>Max(HxW): " + MAX_IMG_H + " x " + MAX_IMG_W +
@@ -179,11 +232,11 @@ function getY(obj)
 function addGlobalStyle(css) {
 	var head, style;
 
-	head = document.getElementsByTagName('head')[0];
+	head = document.getElementsByTagName("head")[0];
 	if (!head) { return; }
 
-	style = document.createElement('style');
-	style.type = 'text/css';
+	style = document.createElement("style");
+	style.type = "text/css";
 	style.innerHTML = css;
 	head.appendChild(style);
 }
@@ -192,8 +245,8 @@ function addGlobalStyle(css) {
 function processImg(img)
 {
 	// Save the oringal H & W first for resize back by the resize buttons
-	if(img.getUserData('origH') == null) { img.setUserData('origH', img.height, null); }
-	if(img.getUserData('origW') == null) { img.setUserData('origW', img.width, null); }
+	if(img.getUserData("origH") == null) { img.setUserData("origH", img.height, null); }
+	if(img.getUserData("origW") == null) { img.setUserData("origW", img.width, null); }
 
 	// Reduce size if image is bigger than the view area
 	if(img.height > MAX_IMG_H || img.width > MAX_IMG_W) {
@@ -206,14 +259,24 @@ function processImg(img)
 			img.height = ADEQUATE_IMG_W * img.getUserData("origH") / img.getUserData("origW");
 		}
 
-		if(img.getUserData('adequateH') == null) { img.setUserData('adequateH', img.height, null); }
-		if(img.getUserData('adequateW') == null) { img.setUserData('adequateW', img.width, null); }
+		if(img.getUserData("adequateH") == null) { img.setUserData("adequateH", img.height, null); }
+		if(img.getUserData("adequateW") == null) { img.setUserData("adequateW", img.width, null); }
 		img.classList.add("resized");
 	}
-	img.setAttribute('tabIndex', 0);
-	displaySizeNoticeMsg(getX(img), getY(img));
-	img.addEventListener('mouseover', sizeNoticeMouseOver, false);
-	img.addEventListener('mouseout', sizeNoticeMouseOut, false);
+
+	// Set tabIndex=0 always for current viewing image
+	img.setAttribute("tabIndex", 0);
+
+	// Bring the image to front
+	img.style.setProperty("position", "relative", "important");
+	img.style.setProperty("z-index", "2147483646", "important");
+
+	// Add event listener for mouse over and out event
+	img.addEventListener("mouseover", sizeNoticeMouseOver, false);
+	img.addEventListener("mouseout", sizeNoticeMouseOut, false);
+
+	// Setup the size buttons but do not display them untill mouse over
+	displaySizeBtns(false, getX(img), getY(img));
 }
 
 function cleanUpImg(img)
@@ -221,125 +284,71 @@ function cleanUpImg(img)
 	// Remove the tabIndex attribute, always set tabIndex=0 only on current viewing image
 	try	{
 		img.removeAttribute("tabIndex");
-		img.removeEventListener('mouseover', sizeNoticeMouseOver, false);
-		img.removeEventListener('mouseout', sizeNoticeMouseOut, false);
+		img.removeEventListener("mouseover", sizeNoticeMouseOver, false);
+		img.removeEventListener("mouseout", sizeNoticeMouseOut, false);
 	} catch(err) {
 	}
 }
 
-function displaySizeNoticeMsg(left, top)
+function displaySizeBtns(display, left, top)
 {
 	if(!sizeNoticeDIV) {
 		// The float message displayed at the top left corner when mouse is over a image
-		sizeNoticeDIV = document.createElement('div');
-		sizeNoticeDIV.style.cssText = "position: absolute !important;\
-								z-index: 9999999999999 !important;\
-								float: none !important;\
-								font-family: Arial, Helvetica !important;\
-								font-size: 9pt !important;\
-								height: auto !important;\
-								width: auto !important;\
-								line-height: 14px !important;\
-								padding: 3px 1px 4px 1px!important;\
-								margin: 0px !important;\
-								border: 0 none !important;\
-								text-align: left !important;\
-								color: #666666 !important;" +
-								"background-color: " + userprefs.curBorderColor + " !important;";
-
+		sizeNoticeDIV = document.createElement("div");
+		sizeNoticeDIV.className = "sizeNoticeDIV";
 		document.body.appendChild(sizeNoticeDIV);
-		sizeNoticeDIV.addEventListener('mouseover', sizeNoticeMouseOver, false);
-		sizeNoticeDIV.addEventListener('mouseout', sizeNoticeMouseOut, false);
+		sizeNoticeDIV.addEventListener("mouseover", sizeNoticeMouseOver, false);
+		sizeNoticeDIV.addEventListener("mouseout", sizeNoticeMouseOut, false);
 
 		// The "Fill Size" button in the float message
-		fillSizeSpan = document.createElement('span');
-		fillSizeSpan.style.cssText = "cursor: pointer !important;\
-										height: auto !important;\
-										width: auto !important;\
-										line-height: 16px !important;\
-										background-color: #F1F1F1 !important;\
-										border: 0 none !important;\
-										margin: 2px !important;\
-										padding: 1px 3px 1px 2px !important;\
-										display: inline !important;";
+		fillSizeSpan = document.createElement("span");
+		fillSizeSpan.className = "sizeBtnSpan";
 		fillSizeSpan.innerHTML = "<u>F</u>ill";
-		fillSizeSpan.addEventListener('click', fillBtnClick, false);
+		fillSizeSpan.addEventListener("click", fillBtnClick, false);
 		sizeNoticeDIV.appendChild(fillSizeSpan);
 
 		// The "Origin Size" button in the float message
-		origSizeSpan = document.createElement('span');
-		origSizeSpan.style.cssText = "cursor: pointer !important;\
-										height: auto !important;\
-										width: auto !important;\
-										line-height: 16px !important;\
-										background-color: #F1F1F1 !important;\
-										border: 0 none !important;\
-										margin: 2px !important;\
-										padding: 1px 2px 1px 2px !important;\
-										display: inline !important;";
+		origSizeSpan = document.createElement("span");
+		origSizeSpan.className = "sizeBtnSpan";
 		origSizeSpan.innerHTML = "<u>O</u>rig";
-		origSizeSpan.addEventListener('click', origBtnClick, false);
+		origSizeSpan.addEventListener("click", origBtnClick, false);
 		sizeNoticeDIV.appendChild(origSizeSpan);
 
 		// The "Natural Size" button in the float message
-		naturalSizeSpan = document.createElement('span');
-		naturalSizeSpan.style.cssText = "cursor: pointer !important;\
-										height: auto !important;\
-										width: auto !important;\
-										line-height: 14px !important;\
-										background-color: #F1F1F1 !important;\
-										border: 0 none !important;\
-										margin: 2px !important;\
-										padding: 1px 2px 1px 2px !important;\
-										display: inline !important;";
+		naturalSizeSpan = document.createElement("span");
+		naturalSizeSpan.className = "sizeBtnSpan";
 		naturalSizeSpan.innerHTML = "<u>N</u>atual";
-		naturalSizeSpan.addEventListener('click', naturalBtnClick, false);
+		naturalSizeSpan.addEventListener("click", naturalBtnClick, false);
 		sizeNoticeDIV.appendChild(naturalSizeSpan);
 
 		// The "Zoom Out" button in the float message
-		zoomOutSizeSpan = document.createElement('span');
-		zoomOutSizeSpan.style.cssText = "cursor: pointer !important;\
-										height: auto !important;\
-										width: auto !important;\
-										line-height: 14px !important;\
-										background-color: #F1F1F1 !important;\
-										border: 0 none !important;\
-										margin: 2px !important;\
-										padding: 1px 2px 1px 2px !important;\
-										display: inline !important;";
+		zoomOutSizeSpan = document.createElement("span");
+		zoomOutSizeSpan.className = "sizeBtnSpan";
 		zoomOutSizeSpan.innerHTML = "<u>Z</u> Out";
-		zoomOutSizeSpan.addEventListener('click', zoomOutBtnClick, false);
+		zoomOutSizeSpan.addEventListener("click", zoomOutBtnClick, false);
 		sizeNoticeDIV.appendChild(zoomOutSizeSpan);
 
 		// The "Zoom In" button in the float message
-		zoomInSizeSpan = document.createElement('span');
-		zoomInSizeSpan.style.cssText = "cursor: pointer !important;\
-										height: auto !important;\
-										width: auto !important;\
-										line-height: 14px !important;\
-										background-color: #F1F1F1 !important;\
-										border: 0 none !important;\
-										margin: 2px !important;\
-										padding: 1px 2px 1px 2px !important;\
-										display: inline !important;";
+		zoomInSizeSpan = document.createElement("span");
+		zoomInSizeSpan.className = "sizeBtnSpan";
 		zoomInSizeSpan.innerHTML = "Z <u>I</u>n";
-		zoomInSizeSpan.addEventListener('click', zoomInBtnClick, false);
+		zoomInSizeSpan.addEventListener("click", zoomInBtnClick, false);
 		sizeNoticeDIV.appendChild(zoomInSizeSpan);
 	};
 
-	sizeNoticeDIV.style.setProperty('left', left + "px", 'important');
-	sizeNoticeDIV.style.setProperty('top', top + "px", 'important');
-	sizeNoticeDIV.style.setProperty('display', 'block', 'important');
+	sizeNoticeDIV.style.setProperty("left", left + "px", "important");
+	sizeNoticeDIV.style.setProperty("top", top + "px", "important");
+	sizeNoticeDIV.style.setProperty("display", (display ? "block" : "none"), "important");
 }
 
 function sizeNoticeMouseOver()
 {
-	sizeNoticeDIV.style.setProperty('display', 'inline', 'important');
+	sizeNoticeDIV.style.setProperty("display", "inline", "important");
 }
 
 function sizeNoticeMouseOut()
 {
-	sizeNoticeDIV.style.setProperty('display', 'none', 'important');
+	sizeNoticeDIV.style.setProperty("display", "none", "important");
 }
 
 function fillBtnClick()
@@ -356,7 +365,7 @@ function fillBtnClick()
 			curImg.height = ADEQUATE_IMG_W * curImg.getUserData("origH") / curImg.getUserData("origW");
 		}
 	}
-	displaySizeNoticeMsg(getX(curImg), getY(curImg));
+	displaySizeBtns(true, getX(curImg), getY(curImg));
 }
 
 function origBtnClick()
@@ -365,7 +374,7 @@ function origBtnClick()
 		curImg.height = curImg.getUserData("origH");
 		curImg.width = curImg.getUserData("origW");
 
-		displaySizeNoticeMsg(getX(curImg), getY(curImg));
+		displaySizeBtns(true, getX(curImg), getY(curImg));
 	}
 }
 
@@ -374,7 +383,7 @@ function naturalBtnClick()
 	curImg.height = curImg.naturalHeight;
 	curImg.width = curImg.naturalWidth;
 
-	displaySizeNoticeMsg(getX(curImg), getY(curImg));
+	displaySizeBtns(true, getX(curImg), getY(curImg));
 }
 
 function zoomOutBtnClick()
@@ -382,7 +391,7 @@ function zoomOutBtnClick()
 	curImg.height *= userprefs.zoomOutStep;
 	curImg.width  *= userprefs.zoomOutStep;
 
-	displaySizeNoticeMsg(getX(curImg), getY(curImg));
+	displaySizeBtns(true, getX(curImg), getY(curImg));
 }
 
 function zoomInBtnClick()
@@ -390,63 +399,41 @@ function zoomInBtnClick()
 	curImg.height *= userprefs.zoomInStep;
 	curImg.width  *= userprefs.zoomInStep;
 
-	displaySizeNoticeMsg(getX(curImg), getY(curImg));
+	displaySizeBtns(true, getX(curImg), getY(curImg));
 }
 
 function debugMsg(html)
 {
 	if(!debugDIV) {
-		debugDIV = document.createElement('div');
-		debugDIV.style.cssText = "position: fixed !important;\
-								z-index: 9999999999999 !important;\
-								float: none !important;\
-								font-family: Arial, Helvetica !important;\
-								font-size: 12px !important;\
-								padding: 2px 5px 2px 5px !important;\
-								background-color: #AF9C90 !important;\
-								border: none !important;\
-								text-align: left !important;\
-								color: #303030 !important;\
-								right: 0 !important;\
-								bottom: 0 !important;";
+		debugDIV = document.createElement("div");
+		debugDIV.className = "debugDIV";
 		document.body.appendChild(debugDIV);
 	};
 	debugDIV.innerHTML = html;
-	debugDIV.style.setProperty('display', 'inline', 'important');
+	debugDIV.style.setProperty("display", "inline", "important");
 
 	if(typeof debugTimeoutID == "number") {
 		window.clearTimeout(debugTimeoutID);
 	}
 	debugTimeoutID = window.setTimeout(function() {
-		debugDIV.style.setProperty('display', 'none', 'important');
+		debugDIV.style.setProperty("display", "none", "important");
 	}, 5000);
 }
 
 function alertMsg(html)
 {
 	if(!alertDIV) {
-		alertDIV = document.createElement('div');
-		alertDIV.style.cssText = "position: fixed !important;\
-								z-index: 9999999999999 !important;\
-								float: none !important;\
-								font-family: Arial, Helvetica !important;\
-								font-size: 16px !important;\
-								padding: 2px 8px 2px 8px !important;\
-								border: none !important;\
-								text-align: left !important;\
-								color: black !important;\
-								left: 0 !important;\
-								bottom: 0 !important;" +
-								"background-color: " + userprefs.curBorderColor + " !important;";
+		alertDIV = document.createElement("div");
+		alertDIV.className = "alertDIV";
 		document.body.appendChild(alertDIV);
 	};
 	alertDIV.innerHTML = html;
-	alertDIV.style.setProperty('display', 'inline', 'important');
+	alertDIV.style.setProperty("display", "inline", "important");
 
 	if(typeof alertTimeoutID == "number") {
 		window.clearTimeout(alertTimeoutID);
 	}
 	alertTimeoutID = window.setTimeout(function() {
-		alertDIV.style.setProperty('display', 'none', 'important');
+		alertDIV.style.setProperty("display", "none", "important");
 	}, 2000);
 }
